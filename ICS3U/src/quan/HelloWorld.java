0261877c2200ffe33a2bfdbaa9ca29f1cf8e3ec1
323013086 <323013086@C-3ZYDYR1.tdsb.on.ca>
2016-03-02 14:44:54 -0500

    Create a HelloWorld file.

---
+++ ICS3U/src/quan/HelloWorld.java
@@ -0,0 +1,15 @@
+package quan;
+/* 
+ * HelloWorld.java
+ * Program prints Hello World to the console
+ * Sharon Quan
+ * 02/03/2016
+ */
+public class HelloWorld { 
+	public static void main(String[] args) {
+		// This prints "Hello world" to the console
+		System.out.println("Hello World. I've reached my programming limits. c:");
+	
+	}
+
+}
