d00af7ba9831651b8f9ec110a2e8c089111e2783
323013086 <323013086@C-3ZYDYR1.tdsb.on.ca>
2016-05-11 14:09:39 -0400

    This program makes the user guess a colour (ie. like hangman

--- ICS3U/src/quan/WordGuess.java
+++ ICS3U/src/quan/WordGuess.java
@@ -1,44 +1,90 @@
 package quan;
-
+/*
+ * Sharon Quan 
+ * 10/05/2016
+ * WordGuess 
+ * This program makes the user guess a colour 
+ */
 import java.util.Scanner;
 
 public class WordGuess {
 
 	public static void main(String[] args) {
 		
-		String [] secretColour = {"red", "blue", "pink","purple"}; //create array
-		String guessWL;  
-		String dashes = ""; 
-		int random = (int)(Math.random() * secretColour.length); 
-		
-		//guessWL = guessWL.substring(0,2) + input + guessWL.substring(3,5); 
-		
-	
+		//declaring variables 
+		String [] secretColour = {"red", "blue", "pink","brown"}; //create array
+		String guessWL;  //user's guessed  word or letter. WL stands for word or letter
+		int random = (int)(Math.random() * secretColour.length); //randomly generating words
+
+		//creating scanner
 		Scanner input = new Scanner(System.in);
-		System.out.println("WordGuess game.");
+		String dashes = ""; 
+		System.out.println("Welcome to WordGuess game.");
 		
-		for (int i  = 0; i < secretColour[random].length(); i++)
+		//creating dashes for the number of letters for the colour from the array 
+		for (int i  = 0; i < secretColour[random].length(); i++) //used like a counter
 		{
 			dashes = dashes + "-"; 
 			
 		}
+		
 		System.out.println(dashes); 
-		do 
+		
+		do //the code inside this loop will repeat until the user guesses all the right letters 
 		{
-			System.out.println("Please enter a letter of a colour or the word of a colour");
+			int counter = 0; //declare counter 
+
+			System.out.println("Please enter a letter of a colour or the word of a colour (Enter ! if you're going to guess a word)");
 			guessWL = input.nextLine(); 
 			
-			if (guessWL == "!")
+			if (guessWL.equalsIgnoreCase("!"))
 			{
+				System.out.println("What colour do you think it is?");
+				guessWL = input.nextLine();
+				
+				if (guessWL.equalsIgnoreCase(secretColour[random])) //seeing if the input matches the word
+				{
+					System.out.println("Wow! You got it right!");
+				}
+				else
+				{
+					System.out.println("Sorry! That's incorrect..");
+				}
 				
 			}
 			else 
 			{
+				int number = secretColour[random].indexOf(guessWL);
+				
+				if(number == -1) //checking if the number is part of the word or not 
+				{
+					System.out.println("Oh no, that letter is not part of the word");
+				}
+				else 
+				{
+					dashes = dashes.substring(0,number) + guessWL + dashes.substring(number+1,secretColour[random].length());
+					System.out.println(dashes);
+
+				}
+				
+				for(int i = 0; i< dashes.length(); i ++) //allows the users to keep on trying
+				{
+					if(dashes.charAt(i) != '-')
+					{
+						counter++; 
+					}
+					if (counter == secretColour[random].length())
+					{
+						System.out.println("Yay! You guessed all the correct letters!!");
+						guessWL = secretColour[random];
+					}
+					
+				}
 				
 			}
-			
-			
-		}while
+						
+		}
+		while(!guessWL.equalsIgnoreCase(secretColour[random])); 
 		
 		
 
