6245d9f83d3467b17b3a9a691ce4a73ee9061074
323013086 <323013086@C-3ZYDYR1.tdsb.on.ca>
2016-05-13 15:37:34 -0400

    This program allows two user to play tick tack toe together on the same computer

--- ICS3U/src/quan/TicTackToe.java
+++ ICS3U/src/quan/TicTackToe.java
@@ -1,6 +1,7 @@
 package quan;
 
 import java.util.Scanner;
+
 /*
  * Sharon Quan
  * 11/05/2016
@@ -9,10 +10,14 @@ import java.util.Scanner;
  */
 public class TicTackToe {
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		// declaring variables and making a two dimensional array
 		String[][] board = new String[3][3];
+
+		// boolean for knowing if the coordinates have been used
+		boolean[][] used = new boolean[3][3];
+		boolean x = false;
+		boolean o = false;
 		// setting coordinates for the board
 		board[0][0] = "";
 		board[0][1] = "";
@@ -24,147 +29,232 @@ public class TicTackToe {
 		board[2][1] = "";
 		board[2][2] = "";
 
-		boolean win = false; // start with making
+		boolean win = false; // start with making win false
 		int row;
 		int column;
+		
+		//determining if the users want to play again
+		boolean again = true;
+		String answer; 
 
 		Scanner input = new Scanner(System.in);
 
-		System.out.println(
-				"Welcome to a game of Tick Tack Toe. Enter the coodinate according to run over rise, please do not enter coodinates that have allready been inputted.");
-		while (win == false) 
+		System.out.println("Welcome to a game of Tick Tack Toe. Enter the coodinate according to run over rise, please do not enter coodinates that have allready been inputted.");
+		//loop for playing again 
+		
+		while (again) 
 		{
-			// the X player's turn
-
-			// asking X player for coordinates
-			System.out.println("What are the coordinates you would like to place the X? (Example: 3, 1)");
-
-			row = input.nextInt();
-			column = input.nextInt();
-
-			// Putting the x into the array
-			board[row - 1][column - 1] = " X ";
-
-			// the tick tack toe "board"
-			System.out.println(board[0][0] + "\t" + board[0][1] + "\t" + board[0][2]);
-			System.out.println(board[1][0] + "\t" + board[1][1] + "\t" + board[1][2]);
-			System.out.println(board[2][0] + "\t" + board[2][1] + "\t" + board[2][2]);
-
-			// Checking for the winner
-			if (board[0][0] == board[0][1] && board[0][0] == board[0][2] && !board[0][2].equals("")) 
+			while (!win) 
 			{
-				System.out.println(board[0][0] + " wins!"); // this line
-															// displays the
-															// player that won
-															// by taking the X
-															// or 0 from the
-															// board
-				win = true;
-			} else if (board[1][0] == board[1][1] && board[1][1] == board[1][2] && !board[1][2].equals("")) 
-			{
-				System.out.println(board[1][0] + " wins!");
-				win = true;
-			} else if (board[2][0] == board[2][1] && board[2][0] == board[2][2] && !board[2][2].equals("")) 
-			{
-				System.out.println(board[2][0] + " wins!");
-				win = true;
-			} else if (board[0][0] == board[1][0] && board[0][0] == board[2][0] && !board[1][0].equals("")) 
-			{
-				System.out.println(board[1][0] + " wins!");
-				win = true;
-			} else if (board[1][0] == board[1][1] && board[1][0] == board[1][2] && !board[1][2].equals("")) 
-			{
-				System.out.println(board[1][0] + " wins!");
-				win = true;
-			} else if (board[2][0] == board[2][1] && board[2][0] == board[2][2] && !board[2][2].equals("")) 
-			{
-				System.out.println(board[2][0] + " wins!");
-				win = true;
-			} else if (board[0][0] == board[1][1] && board[0][0] == board[2][2] && !board[2][2].equals("")) 
-			{
-				System.out.println(board[2][2] + " wins!");
-				win = true;
-			} else if (board[0][2] == board[1][1] && board[0][2] == board[2][0] && !board[0][2].equals("")) 
-			{
-				System.out.println(board[0][0] + " wins!");
-				win = true;
-			} else 
-			{
-				win = false;
-			}
-			// seeing if it's a cat's game
-			if (!board[0][0].equals("") && board[0][1].equals("") && board[0][2].equals("") && board[1][0].equals("")
-					&& board[1][1].equals("") && board[1][2].equals("") && board[2][0].equals("")
-					&& board[2][1].equals("") && board[2][2].equals("")) 
-			{
-				win = true;
-				System.out.println("Oh no! It's a cat's game!");
-			}
-			// the O player's turn
-			if (!win) 
-			{
-				// asking O player for coordinates
-				System.out.println("What are the coordinates you would like to place the O? (Example: 3 1)");
+				// the X player's turn
+				if (!o)
+				{
+				// asking X player for coordinates
+				System.out.println("What are the coordinates you would like to place the X? (Example: 3, 1)");
 
 				row = input.nextInt();
 				column = input.nextInt();
 
-				// Putting the O into the array
-				board[row - 1][column - 1] = " O ";
+				// making sure that the coordinates aren't used twice
+				if (!used[row - 1 ][column - 1]) 
+				{
+					used[row - 1][column - 1] = true;
 
-				System.out.println(board[0][0] + "\t" + board[0][1] + "\t" + board[0][2]);
-				System.out.println(board[1][0] + "\t" + board[1][1] + "\t" + board[1][2]);
-				System.out.println(board[2][0] + "\t" + board[2][1] + "\t" + board[2][2]);
+					// Putting the x into the array
+					board[row - 1][column - 1] = " X ";
 
-				// Checking for the winner again
-				if (board[0][0] == board[0][1] && board[0][0] == board[0][2] && !board[0][2].equals("")) 
+					// the tick tack toe "board"
+					System.out.println(board[0][0] + "\t" + board[0][1] + "\t" + board[0][2]);
+					System.out.println(board[1][0] + "\t" + board[1][1] + "\t" + board[1][2]);
+					System.out.println(board[2][0] + "\t" + board[2][1] + "\t" + board[2][2]);
+
+					// Checking for the winner
+					if (board[0][0] == board[0][1] && board[0][0] == board[0][2] && !board[0][2].equals("")) 
+					{
+						System.out.println(board[0][0] + " wins!"); 
+						win = true;
+					} 
+					else if (board[1][0] == board[1][1] && board[1][1] == board[1][2] && !board[1][2].equals("")) 
+					{
+						System.out.println(board[1][0] + " wins!");
+						win = true;
+					} 
+					else if (board[2][0] == board[2][1] && board[2][0] == board[2][2] && !board[2][2].equals("")) 
+					{
+						System.out.println(board[2][0] + " wins!");
+						win = true;
+					} 
+					else if (board[0][1] == board[1][1] && board[1][1] == board[2][1] && !board[2][1].equals("")) 
+					{
+						System.out.println(board[0][1] + " wins!"); //CHANGED
+						win = true;
+					} 
+					else if (board[0][0] == board[0][1] && board[0][1] == board[0][2] && !board[0][2].equals("")) 
+					{
+						System.out.println(board[0][0] + " wins!"); //CHECK
+						win = true;
+					} 
+					else if (board[0][2] == board[1][2] && board[1][2] == board[2][2] && !board[2][2].equals("")) 
+					{
+						System.out.println(board[0][2] + " wins!"); //CHECK
+						win = true;
+					} 
+					else if (board[0][0] == board[1][1] && board[0][0] == board[2][2] && !board[2][2].equals("")) 
+					{
+						System.out.println(board[2][2] + " wins!");
+						win = true;
+					} 
+					else if (board[0][2] == board[1][1] && board[0][2] == board[2][0] && !board[0][2].equals("")) 
+					{
+						System.out.println(board[0][0] + " wins!");
+						win = true;
+					} 
+					else 
+					{
+						win = false;
+					}
+					// seeing if it's a cat's game
+					if (!board[0][0].equals("") && !board[0][1].equals("") && !board[0][2].equals("")
+							&& !board[1][0].equals("") && !board[1][1].equals("") && !board[1][2].equals("")
+							&& !board[2][0].equals("") && !board[2][1].equals("") && !board[2][2].equals("") && !win) 
+					{
+						win = true;
+						System.out.println("Oh no! It's a cat's game!");
+					}
+					x = false;
+
+				} else 
 				{
-					System.out.println(board[0][0] + " wins!");
-					win = true;
-				} else if (board[1][0] == board[1][1] && board[1][1] == board[1][2] && !board[1][2].equals("")) 
-				{
-					System.out.println(board[1][0] + " wins!");
-					win = true;
-				} else if (board[2][0] == board[2][1] && board[2][0] == board[2][2] && !board[2][2].equals("")) 
-				{
-					System.out.println(board[2][0] + " wins!");
-					win = true;
-				} else if (board[0][0] == board[1][0] && board[0][0] == board[2][0] && !board[1][0].equals("")) 
-				{
-					System.out.println(board[1][0] + " wins!");
-					win = true;
-				} else if (board[1][0] == board[1][1] && board[1][0] == board[1][2] && !board[1][2].equals("")) 
-				{
-					System.out.println(board[1][0] + " wins!");
-					win = true;
-				} else if (board[2][0] == board[2][1] && board[2][0] == board[2][2] && !board[2][2].equals("")) 
-				{
-					System.out.println(board[2][0] + " wins!");
-					win = true;
-				} else if (board[0][0] == board[1][1] && board[0][0] == board[2][2] && !board[2][2].equals("")) 
-				{
-					System.out.println(board[2][2] + " wins!");
-					win = true;
-				} else if (board[0][2] == board[1][1] && board[0][2] == board[2][0] && !board[0][2].equals("")) 
-				{
-					System.out.println(board[0][0] + " wins!");
-					win = true;
-				} else {
-					win = false;
+					System.out.println("Sorry, those coordinates have already been used.");
+					x = true;
 				}
-				// seeing if it's a cat's game
-
-				if (!board[0][0].equals("") && board[0][1].equals("") && board[0][2].equals("")
-						&& board[1][0].equals("") && board[1][1].equals("") && board[1][2].equals("")
-						&& board[2][0].equals("") && board[2][1].equals("") && board[2][2].equals("")) 
-				{
-					win = true;
-					System.out.println("Oh no! It's a cat's game!");
 				}
+				// the O player's turn
+				if (!win && !x) 
+				{
+					// asking O player for coordinates
+					System.out.println("What are the coordinates you would like to place the O? (Example: 3 1)");
+
+					row = input.nextInt();
+					column = input.nextInt();
+
+					if (!used[row-1][column-1]) 
+					{
+						used[row-1][column-1] = true;
+
+						// Putting the O into the array
+						board[row - 1][column - 1] = " O ";
+
+						System.out.println(board[0][0] + "\t" + board[0][1] + "\t" + board[0][2]);
+						System.out.println(board[1][0] + "\t" + board[1][1] + "\t" + board[1][2]);
+						System.out.println(board[2][0] + "\t" + board[2][1] + "\t" + board[2][2]);
+
+						// Checking for the winner again
+						if (board[0][0] == board[0][1] && board[0][0] == board[0][2] && !board[0][2].equals("")) 
+						{
+							System.out.println(board[0][0] + " wins!"); 
+							win = true;
+						} 
+						else if (board[1][0] == board[1][1] && board[1][1] == board[1][2] && !board[1][2].equals("")) 
+						{
+							System.out.println(board[1][0] + " wins!");
+							win = true;
+						} 
+						else if (board[2][0] == board[2][1] && board[2][0] == board[2][2] && !board[2][2].equals("")) 
+						{
+							System.out.println(board[2][0] + " wins!");
+							win = true;
+						} 
+						else if (board[0][1] == board[1][1] && board[1][1] == board[2][1] && !board[2][1].equals("")) 
+						{
+							System.out.println(board[0][1] + " wins!"); //CHANGED
+							win = true;
+						} 
+						else if (board[0][0] == board[0][1] && board[0][1] == board[0][2] && !board[0][2].equals("")) 
+						{
+							System.out.println(board[0][0] + " wins!"); //CHECK
+							win = true;
+						} 
+						else if (board[0][2] == board[1][2] && board[1][2] == board[2][2] && !board[2][2].equals("")) 
+						{
+							System.out.println(board[0][2] + " wins!"); //CHECK 
+							win = true;
+						} 
+						else if (board[0][0] == board[1][1] && board[0][0] == board[2][2] && !board[2][2].equals("")) 
+						{
+							System.out.println(board[2][2] + " wins!");
+							win = true;
+						} 
+						else if (board[0][2] == board[1][1] && board[0][2] == board[2][0] && !board[0][2].equals("")) 
+						{
+							System.out.println(board[0][0] + " wins!");
+							win = true;
+						} 
+						else 
+						{
+							win = false;
+						}
+						// seeing if it's a cat's game
+
+						if (!board[0][0].equals("") && !board[0][1].equals("") && !board[0][2].equals("")
+								&& !board[1][0].equals("") && !board[1][1].equals("") && !board[1][2].equals("")
+								&& !board[2][0].equals("") && !board[2][1].equals("") && !board[2][2].equals("") && !win) 
+						{
+							win = true;
+							System.out.println("Oh no! It's a cat's game!");
+						}
+						o = false;
+					} 
+					else 
+					{
+						System.out.println("Sorry, those coordinates have already been used.");
+						o = true;
+					}
+				}
+
+			}
+			
+			System.out.println("Play again? (Please enter Y or N)");
+			
+			input.nextLine(); 
+			answer = input.nextLine(); 
+			// seeing if the player wants to play again 
+			if(answer.equalsIgnoreCase("n"))
+			{
+				System.out.println("Oh, okay. Bye!"); 
+				again = false; 
+			}
+			//resetting the board and coordinates 
+			else 
+			{	
+				board[0][0] = "";
+				board[0][1] = "";
+				board[0][2] = "";
+				board[1][0] = "";
+				board[1][1] = "";
+				board[1][2] = "";
+				board[2][0] = "";
+				board[2][1] = "";
+				board[2][2] = "";
+				
+				used[0][0] = false;
+				used[0][1] = false;
+				used[0][2] = false;
+				used[1][0] = false;
+				used[1][1] = false;
+				used[1][2] = false;
+				used[2][0] = false;
+				used[2][1] = false;
+				used[2][2] = false;
+				
+				win = false; 
+				again = true; 
+				x = false; 
+				o = false; 
+				
 			}
 
 		}
-
 	}
 
 }
