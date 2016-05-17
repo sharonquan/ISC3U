c752b18c628932f6d72d6ff99a05caf49cec5e0b
323013086 <323013086@C-3ZYDYR1.tdsb.on.ca>
2016-05-17 15:01:08 -0400

    This program uses methods to spell out Mississippi

---
+++ ICS3U/src/quan/Mississippi.java
@@ -0,0 +1,75 @@
+package quan;
+/* Mississippi.java
+ * This program uses methods to spell out Mississippi
+ * Sharon Quan
+ * 2016 05 17
+ */
+public class Mississippi {
+	public static void printM()
+	{
+		System.out.println("M       M");
+		System.out.println("MM     MM");
+		System.out.println("M M   M M");
+		System.out.println("M   M   M");
+		System.out.println("M       M");
+		System.out.println("M       M");
+		System.out.println("M       M");
+		System.out.println(); 
+
+	}
+	public static void printI()
+	{
+		System.out.println("IIIII");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("IIIII");
+		System.out.println(); 
+
+	}
+	public static void printS()
+	{
+		System.out.println(" SSSSS");
+		System.out.println("S     S");
+		System.out.println("S");
+		System.out.println(" SSSSS");
+		System.out.println("      S");
+		System.out.println("S     S");
+		System.out.println(" SSSSS");
+		System.out.println(); 
+
+	}
+	public static void printP()
+	{
+		System.out.println("PPPPP");
+		System.out.println("P   PP");
+		System.out.println("P   PP");
+		System.out.println("P   PP");
+		System.out.println("PPPPP");
+		System.out.println("P");
+		System.out.println("P");
+		System.out.println("P");
+		System.out.println(); 
+
+	}
+
+	public static void main(String[] args) 
+	{
+		printM();
+		printI();
+		printS();
+		printS();
+		printI(); 
+		printS();
+		printS();
+		printI();
+		printP(); 
+		printP(); 
+		printI();
+		
+	}
+
+}
